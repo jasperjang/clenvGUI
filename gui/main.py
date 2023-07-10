c7972cda4d87224a9d433e986ac1c24093927a83
jasperjang <jasper.jang@brainco.tech>
2023-07-10 14:21:43 -0400

    file rearrangement

--- gui/main.py
+++ gui/main.py
@@ -135,11 +135,11 @@ def exec_config(run_config):
     Task.enqueue(create_populate.task, queue_name=queue)
     if tags != ['']:
         create_populate.task.set_tags(tags)
-    URL = create_populate.task.get_output_log_web_page()
     main_window['exec_complete_text1'].update(f"New task created id={task_id}")
     main_window['exec_complete_text2'].update(f"Task id={task_id} sent for execution on queue {queue}")
     main_window['exec_layout'].update(visible=False)
     main_window['exec_complete_layout'].update(visible=True)
+    return create_populate.task
 
 ################################################################################
 ######                          Initialization                            ######
@@ -159,138 +159,21 @@ sg.LOOK_AND_FEEL_TABLE['clearML'] = {
 
 sg.theme('clearML')
 
-queue_list = get_queue_list()
-task_types = [
-    "training",
-    "testing",
-    "inference",
-    "data_processing",
-    "application",
-    "monitor",
-    "controller",
-    "optimizer",
-    "service",
-    "qc",
-    "other"
-]
-task_id = ''
-queue = ''
-URL = ''
-
 config_manager = ConfigManager('~/.clenv-config-index.json')
-active_profile = config_manager.get_active_profile()[0]
-non_active_profiles = config_manager.get_non_active_profiles()
-non_active_profile_names = get_non_active_profile_names(non_active_profiles)
-profile_list = get_profile_list(active_profile, non_active_profiles)
-profile_string = get_profile_string(profile_list)
 
-# Secondary layouts
-main_layout = [
-    [sg.Button('    Task Execution    ', font='Ariel 18', key='task_exec')],
-    [sg.Button('Profile Configuration', font='Ariel 18', key='config')]
-]
-
-run_template_layout = [
-    [sg.Text('Select a template or run new:')],
-    [sg.Listbox(['No templates created'], key='template_chosen', size=(60,9))],
-    [sg.Button('Run New', key='run_template_new'),
-     sg.Button('Run Template', key='run_template_template'),
-     sg.Button('Delete Template', key='run_template_delete'),
-     sg.Button('Back', key='run_template_back')]
-]
-
-exec_layout = [
-    [sg.Text('Please choose a queue to execute the task')],
-    [sg.OptionMenu(queue_list, key='queue_list')],
-    [sg.Text('')],
-    [sg.Text('Please choose a task type')],
-    [sg.OptionMenu(task_types, key='task_types')],
-    [sg.Text('')],
-    [sg.Text('Please enter a task name')],
-    [sg.InputText('', key='task_name')],
-    [sg.Text('')],
-    [sg.Text('Please enter a script path')],
-    [sg.InputText('/', key='path')],
-    [sg.Text('')],
-    [sg.Text('Enter tags separated by commas:')],
-    [sg.InputText('', key='tags')],
-    [sg.Checkbox('Save as template?', key='save_as_template')],
-    [sg.Button('Confirm', key='exec_confirm'), 
-     sg.Button('Back', key='exec_back')]
-]
-
-exec_complete_layout = [
-    [sg.Text(f"New task created id={task_id}", key='exec_complete_text1')],
-    [sg.Text(f"Task id={task_id} sent for execution on queue {queue}", key='exec_complete_text2')],
-    [sg.Text("Execution log at:")],
-    [sg.Button('Navigate to project on clearML', key='exec_complete_URL')],
-    [sg.Button('Back', key='exec_complete_back')]
-]
-
-config_layout = [
-    [sg.Text('Profile Configuration Options:')],
-    [sg.OptionMenu(['Checkout a Profile',
-                    'Create a Profile',
-                    'Delete a Profile',
-                    'List of Profiles',
-                    'Rename a Profile',
-                    'Configure API Path'], key='config_options')],
-    [sg.Button('Confirm', key='config_confirm'), 
-     sg.Button('Back', key='config_back')]
-]
-
-config_checkout_layout = [
-    [sg.Text('Select a profile to checkout:')],
-    [sg.Text(f'Active Profile: {active_profile}', key='checkout_active_profile')],
-    [sg.OptionMenu([{}], key='checkout_non_active_profiles')],
-    [sg.Button('Confirm', key='config_checkout_confirm'), 
-     sg.Button('Back', key='config_checkout_back')]
-]
-
-config_create_layout = [
-    [sg.Text('Enter a new profile name:')],
-    [sg.InputText('', key='new_profile_name')],
-    [sg.Button('Confirm', key='config_create_confirm'),
-     sg.Button('Back', key='config_create_back')]
-]
-
-config_delete_layout = [
-    [sg.Text('Select a profile to delete:')],
-    [sg.OptionMenu([{}], key='delete_non_active_profiles')],
-    [sg.Button('Confirm', key='config_delete_confirm'),
-     sg.Button('Back', key='config_delete_back')]
-]
-
-config_list_layout = [
-    [sg.Text('List of profiles:')],
-    [sg.Text(profile_string, key='profile_list')],
-    [sg.Button('Back', key='config_list_back')]
-]
-
-config_rename_layout = [
-    [sg.Text('Select a profile to rename:')],
-    [sg.OptionMenu(profile_list, key='profile_list_menu')],
-    [sg.Text('')],
-    [sg.Text('Enter a new name:')],
-    [sg.InputText('', key='profile_rename')],
-    [sg.Button('Confirm', key='config_rename_confirm'),
-     sg.Button('Back', key='config_rename_back')]
-]
-
-config_configure_layout = [
-    [sg.Text('Select a profile to reconfigure:')],
-    [sg.OptionMenu(profile_list, key='profile_to_config')],
-    [sg.Text('')],
-    [sg.Text('Enter a multiline configuration below:')],
-    [sg.Text('''
-This can be found by navigating to the clearML website, 
-clicking the button in the top right corner, then 
-settings > workspace > create new credentials
-             ''')],
-    [sg.Multiline('', key='multiline_config', size=(60,9))],
-    [sg.Button('Confirm', key='config_configure_confirm'),
-     sg.Button('Back', key='config_configure_back')]
-]
+from layouts import (
+    main_layout, 
+    run_template_layout, 
+    exec_layout, 
+    exec_complete_layout, 
+    config_layout, 
+    config_checkout_layout, 
+    config_create_layout, 
+    config_delete_layout, 
+    config_list_layout, 
+    config_rename_layout, 
+    config_configure_layout
+)
 
 # Main layout
 layout = [
@@ -441,11 +324,15 @@ while True:
 
         # run template controllers
         if main_event == 'run_template_new':
+            queue_list = get_queue_list()
+            main_window['queue_list'].update(values=queue_list)
             main_window['save_as_template'].update(False)
             main_window['run_template_layout'].update(visible=False)
             main_window['exec_layout'].update(visible=True)
         if main_event == 'run_template_template':
             if main_values['template_chosen'] != {} and main_values['template_chosen'] != []:
+                queue_list = get_queue_list()
+                main_window['queue_list'].update(values=queue_list)
                 template_name = main_values['template_chosen'][0]
                 with open("task_templates.json", "r") as f:
                     current_templates = json.load(f)
@@ -528,7 +415,8 @@ while True:
                     new_template_window = sg.Window('Template Creation', new_template_layout, modal=True)
                     set_active_window('new_template')
                 else:
-                    exec_config(run_config)
+                    task = exec_config(run_config)
+                    URL = task.get_output_log_web_page()
         if main_event == 'exec_complete_URL':
             wb.open(URL)
         if main_event == 'exec_complete_back':
@@ -576,7 +464,8 @@ while True:
             main_window['template_chosen'].update(values=template_names)
             set_active_window('main')
             new_template_window.close()
-            exec_config(run_config)
+            task = exec_config(run_config)
+            URL = task.get_output_log_web_page()
         if new_template_event == sg.WIN_CLOSED:
             set_active_window('main')
             new_template_window.close()
